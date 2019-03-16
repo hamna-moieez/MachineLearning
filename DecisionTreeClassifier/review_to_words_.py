42cc780d15f7443907e9793b37933ba0decd2949
hamna-moieez <hamna21797@gmail.com>
2019-03-16 17:13:21 +0500

    DecisionTreeCommit

---
+++ DecisionTreeClassifier/review_to_words_.py
@@ -0,0 +1,30 @@
+import re
+from bs4 import BeautifulSoup 
+import nltk
+from nltk.corpus import stopwords
+
+def review_to_words( raw_review ):
+
+    # Function to convert a raw review to a string of words
+    # The input is a single string (a raw movie review), and 
+    # the output is a single string (a preprocessed movie review)
+    #
+    # 1. Remove HTML
+    review_text = BeautifulSoup(raw_review).get_text() 
+    #
+    # 2. Remove non-letters        
+    letters_only = re.sub("[^a-zA-Z]", " ", review_text) 
+    #
+    # 3. Convert to lower case, split into individual words
+    words = letters_only.lower().split()                             
+    #
+    # 4. In Python, searching a set is much faster than searching
+    #   a list, so convert the stop words to a set
+    stops = set(stopwords.words("english"))                  
+    # 
+    # 5. Remove stop words
+    meaningful_words = [w for w in words if not w in stops]   
+    #
+    # 6. Join the words back into one string separated by space, 
+    # and return the result.
+    return( " ".join( meaningful_words )) 
